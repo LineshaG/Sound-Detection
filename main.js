75e133258a0baa5e40bb8130e74578c7d9aae99c
LineshaG <88967931+LineshaG@users.noreply.github.com>
2023-01-11 19:43:48 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-https://teachablemachine.withgoogle.com/models/pz5-o8GiU/
+function startClassification()
+{
+   navigator.mediaDevices.getUserMedia({audio: true});
+   classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/pz5-o8GiU/model.json', modelReady);
+}
+
+function modelReady()
+{
+    classifier.classify(gotResult);
+}
+
+function gotResult(error,result)
+{
+    if (error)
+    {
+        console.error(error);
+    }
+    else
+    {
+        console.log(result);
+        document.getElementById("label").innerHTML = 'I can hear - '+result[0].label;
+        document.getElementById("confidence").innerHTML = 'Accuracy - '+ (result[0].confidence*100).toFixed(2)+"%"; 
+    }
+}
